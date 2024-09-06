edf802de4eb3a1bf1f7485072e29e018466981c7
anthonyisensee <60198052+anthonyisensee@users.noreply.github.com>
2024-09-05 21:36:53 -0400

    feat: event default date set to current date

--- src/DateHelperFunctions.js
+++ src/DateHelperFunctions.js
@@ -52,7 +52,7 @@ export function currentInputDate() {
     const date = new Date()
     const yyyy = String(date.getFullYear())
     const mm = String(date.getMonth() + 1).padStart(2, "0")
-    const dd = String(date.getDay()).padStart(2, "0")
+    const dd = String(date.getDate()).padStart(2, "0")
     return `${yyyy}-${mm}-${dd}`
 
 }
