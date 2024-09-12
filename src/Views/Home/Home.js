f35418930ed279194fff5aab9907b6c955ebbb8d
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2024-09-12 08:06:15 -0400

    chore: updates home page text

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
     return (
         <div className="content">
             <h1>Event Tracker</h1>
-            <p>Event tracker is the modern equivalent of the "It has been <span className="is-underlined">&nbsp;X&nbsp;</span> days since the last accident" whiteboard. It is designed to be a multipurpose tool to help you track, view, and visualize both the time since events have occurred as well as the frequency of those events.</p>
-            <Link to="/tracker" className="button is-success">Create a Tracker</Link>
+            <p>Event tracker is the modern equivalent of the "It has been <span className="is-underlined">&nbsp;X&nbsp;</span> days since the last accident" whiteboard. It is designed to help you easily visualize the time since an event has occurred by allowing you to create custom trackers.</p>
+            <p>You can get started by <Link to="/tracker">creating a tracker</Link> or visiting the <Link to="/dashboard">dashboard</Link>.</p>
         </div>
     )
 
