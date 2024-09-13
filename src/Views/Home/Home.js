9210c24bbe403f294e64374584da7939a755df51
anthonyisensee <60198052+anthonyisensee@users.noreply.github.com>
2024-09-12 21:17:01 -0400

    chore: homepage text more descriptive

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="content">
             <h1>Event Tracker</h1>
-            <p>Event tracker is the modern equivalent of the "It has been <span className="is-underlined">&nbsp;X&nbsp;</span> days since the last accident" whiteboard. It is designed to help you easily visualize the time since an event has occurred by allowing you to create custom trackers.</p>
+            <p>Event tracker is the modern equivalent of the "It has been <span className="is-underlined">&nbsp;X&nbsp;</span> days since the last accident" whiteboard. It is designed to help you easily visualize the time since an event has occurred by allowing you to create custom trackers and log events against them.</p>
             <p>You can get started by <Link to="/tracker">creating a tracker</Link> or visiting the <Link to="/dashboard">dashboard</Link>.</p>
         </div>
     )
