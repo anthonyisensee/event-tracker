82c77e8b2d500b212c7c56c21a4290bbf4ce5416
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2024-10-14 09:31:31 -0400

    chore: remove unused code

--- src/Views/Dashboard/TrackerCard.js
+++ src/Views/Dashboard/TrackerCard.js
@@ -1,50 +1,9 @@
-import { useEffect, useState, useCallback } from "react"
 import { Link, useLocation } from "react-router-dom"
-import { getEventDate, timeSinceDateArray } from "../../DateHelperFunctions"
-import { getLastEventWithTrackerId } from "../../IndexedDB/IndexedDB"
 import TimeDisplay from "../../Shared/TimeDisplay"
 
 const TrackerCard = ({ tracker }) => {
 
-    const [latestEvent, setLatestEvent] = useState()
-    const [timeSinceArray, setTimeSinceArray] = useState(timeSinceDateArray(null))
-    const [timeBetweenObject, setTimeBetweenObject] = useState()
-
     const location = useLocation()
-
-    const getAndSetLatestEvent = useCallback(async (trackerId) => {
-
-        getLastEventWithTrackerId(trackerId)
-            .then(event => setLatestEvent(event))
-            .catch(error => console.error(error))
-
-    }, [])
-
-    useEffect(() => {
-
-        getAndSetLatestEvent(tracker.id)
-
-    }, [tracker.id, getAndSetLatestEvent])
-
-    useEffect(() => {
-
-        // Don't do anything until the latestEvent has been set
-        if (latestEvent) {
-
-            // Update timeSinceArray for the first time
-            setTimeSinceArray(timeSinceDateArray(getEventDate(latestEvent)))
-
-            // Set an interval to update the timeSinceArray subsequent times
-            const interval = setInterval(() => {
-                setTimeSinceArray(timeSinceDateArray(getEventDate(latestEvent)))
-            }, 1000)
-
-            // Clean up the interval when the component unmounts
-            return () => clearInterval(interval)
-
-        }
-
-    }, [latestEvent])
 
     return (
         <div className="box is-flex is-flex-direction-column is-justify-content-space-between" style={{ height: "100%" }}>
